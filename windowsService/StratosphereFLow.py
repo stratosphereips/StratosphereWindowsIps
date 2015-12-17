f8f170a815abe1ead1d915b7626343d875cb1966
Frenky <strasfra@fel.cvut.cz>
2015-12-17 12:24:27 +0100

    It is first flow to reading flow from stdin

--- windowsService/StratosphereFLow.py
+++ windowsService/StratosphereFLow.py
@@ -21,19 +21,26 @@ class ct(Thread):
 
     def run(self):
         # Thread 1
+
         if self.name is "t1":
+            index = 0
             for line in sys.stdin:
                 print 'Flow', line
                 flow_queue.put(line)
                 # time.sleep(1)
+                index += 1
+            # print 'Thread1:',index
 
         # Thread 2 - getting flow from quene and calling functin from Stratospehre Detector
         if self.name is "t2":
+            index2 = 0
             while (True):
                 flow = flow_queue.get()
                 print 'Getting flow from quene...'
                 StratosphereDetector.analyze_flow(flow)
-                time.sleep(1)
+                #time.sleep(1)
+                index2 += 1
+                # print 'Thread2:',index2
 
 
 

71e23984b606cc6c9e1ca5aab6851a3ad1f32520
Frenky <strasfra@fel.cvut.cz>
2015-12-30 02:07:04 +0100

    Just clean it

--- windowsService/StratosphereDetector.py
+++ windowsService/StratosphereDetector.py
@@ -1,19 +1,10 @@
 __author__ = 'Frenky'
 
-
-
-
-###
-# just One thread
-#
-##
-
 def analyze_flow(flow):
-    pass
-    #print 'Analyzing flow:',flow
+    # pass
+    print 'Analyzing flow:',flow
     # spliting flow
     # for x in flow.split(','):
     #     print x
-
 
 

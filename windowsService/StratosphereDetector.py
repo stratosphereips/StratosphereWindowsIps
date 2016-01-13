1634373203e155cfb9f2b00b982d7d57eb9e3a61
Frenky <strasfra@fel.cvut.cz>
2016-01-13 22:17:10 +0100

    Add radnom function for normall and botnet.

--- windowsService/StratosphereDetector.py
+++ windowsService/StratosphereDetector.py
@@ -1,7 +1,9 @@
 __author__ = 'Frenky'
-
-def analyze_flow(flow):
-    print 'Analyzing flow:',flow
+import random
 
 
-
+def detect(state):
+    label_number = random.randint(1,4)
+    if label_number is 1:
+        return 'NETBOT'
+    return 'NORMAL'

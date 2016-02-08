93ed95cb1e5213ffea68b5081a38ec17d373754d
Frenky <strasfra@fel.cvut.cz>
2016-02-08 18:35:36 +0100

    Add comments.

--- StratosphereWindowsIPS/StratosphereConfig.py
+++ StratosphereWindowsIPS/StratosphereConfig.py
@@ -32,47 +32,56 @@ class StratosphereConfig:
             self.run_on_start = True
             # STRATOSPHERE OUTPUT
             self.verbose_option = 3
+            self.config = None
 
         def check_config(self):
-
             file_name = 'configfile.cfg'
-            # ConfigParser.ConfigParser.add_comment = lambda self, section, option, value: self.set(section, '; '+option, value)
-            config = ConfigParser.ConfigParser()
-            config.read(file_name)
+            self.config = ConfigParser.ConfigParser(allow_no_value = True)
+
+            self.config.read(file_name)
             try:
-                self.days_update_again = config.getint('STRATOSPHERE_WINDOW', 'DAYS_UPDATE_AGAIN')
-                self.is_forbidden = config.getboolean('STRATOSPHERE_WINDOW', 'DONT_UPDATE')
-                self.date_string = config.get('STRATOSPHERE_WINDOW', 'DATE_OF_NEXT_UPDATE')
-                self.url_to_classes = config.get('STRATOSPHERE_WINDOW', 'UPDATE_URL_CLASSES')
-                self.check_if_process_work = config.getint('STRATOSPHERE_WINDOW', 'CHECK_IF_UP_EVERY')
-                self.length_of_state = config.getint('STRATOSPHERE_WINDOW', 'LENGTH_OF_STATE')
-                self.time_windows_length = config.getint('STRATOSPHERE_WINDOW', 'TIME_WINDOWS_LENGTH')
-                self.verbose_option = config.getint('STRATOSPHERE_OUTPUT', 'VERBOSE_OPTION')
-                self.url_to_modules = config.get('STRATOSPHERE_WINDOW', 'UPDATE_URL_MODULES')
-                self.url_to_models = config.get('STRATOSPHERE_WINDOW', 'UPDATE_URL_MODELS')
+                self.days_update_again = self.config.getint('STRATOSPHERE_WINDOW', 'DAYS_UPDATE_AGAIN')
+                self.is_forbidden = self.config.getboolean('STRATOSPHERE_WINDOW', 'DONT_UPDATE')
+                self.date_string = self.config.get('STRATOSPHERE_WINDOW', 'DATE_OF_NEXT_UPDATE')
+                self.url_to_classes = self.config.get('STRATOSPHERE_WINDOW', 'UPDATE_URL_CLASSES')
+                self.check_if_process_work = self.config.getint('STRATOSPHERE_WINDOW', 'CHECK_IF_UP_EVERY')
+                self.length_of_state = self.config.getint('STRATOSPHERE_WINDOW', 'LENGTH_OF_STATE')
+                self.time_windows_length = self.config.getint('STRATOSPHERE_WINDOW', 'TIME_WINDOWS_LENGTH')
+                self.verbose_option = self.config.getint('STRATOSPHERE_OUTPUT', 'VERBOSE_OPTION')
+                self.url_to_modules = self.config.get('STRATOSPHERE_WINDOW', 'UPDATE_URL_MODULES')
+                self.url_to_models = self.config.get('STRATOSPHERE_WINDOW', 'UPDATE_URL_MODELS')
 
             except:
                 # FIRST SECTION
-                config.add_section('STRATOSPHERE_WINDOW')
-                # config.add_comment('STRATOSPHERE_WINDOW', 'Updating (True or False)', 3)
-                config.set('STRATOSPHERE_WINDOW', 'DONT_UPDATE',  self.is_forbidden)
-                config.set('STRATOSPHERE_WINDOW', 'DATE_OF_NEXT_UPDATE', self.date_string)
-                config.set('STRATOSPHERE_WINDOW', 'DAYS_UPDATE_AGAIN', self.days_update_again)
-                config.set('STRATOSPHERE_WINDOW', 'CHECK_IF_UP_EVERY', self.check_if_process_work)
-                config.set('STRATOSPHERE_WINDOW', 'UPDATE_URL_CLASSES', self.url_to_classes)
-                config.set('STRATOSPHERE_WINDOW', 'LENGTH_OF_STATE', self.length_of_state)
-                config.set('STRATOSPHERE_WINDOW', 'TIME_WINDOWS_LENGTH', self.time_windows_length)
-                config.set('STRATOSPHERE_WINDOW', 'UPDATE_URL_MODULES', self.url_to_modules)
-                config.set('STRATOSPHERE_WINDOW', 'UPDATE_URL_MODELS', self.url_to_models)
+                self.config.add_section('STRATOSPHERE_WINDOW')
+
+                self.config.set('STRATOSPHERE_WINDOW', '; IF YOU WANT TO UPDATE THIS APPLICATION DONT_UPDATE = False, OTHERWISE DONT_UPDATE = True.')
+                self.config.set('STRATOSPHERE_WINDOW', 'DONT_UPDATE',  self.is_forbidden)
+                self.config.set('STRATOSPHERE_WINDOW', '; DATE OF NEXT UPDATE (MODULES, MODELS, APPLICATION).')
+                self.config.set('STRATOSPHERE_WINDOW', 'DATE_OF_NEXT_UPDATE', self.date_string)
+                self.config.set('STRATOSPHERE_WINDOW', '; NUMBER OF DAYS, WHEN NEXT UPADTE WILL BE.')
+                self.config.set('STRATOSPHERE_WINDOW', 'DAYS_UPDATE_AGAIN', self.days_update_again)
+                self.config.set('STRATOSPHERE_WINDOW', '; NUMBER IN SECONDS, WHICH CHECK IF THE PROCESS FOR DETECTING RUNS.')
+                self.config.set('STRATOSPHERE_WINDOW', 'CHECK_IF_UP_EVERY', self.check_if_process_work)
+                self.config.set('STRATOSPHERE_WINDOW', '; URL FOR UPDATING.')
+                self.config.set('STRATOSPHERE_WINDOW', 'UPDATE_URL_CLASSES', self.url_to_classes)
+                self.config.set('STRATOSPHERE_WINDOW', 'UPDATE_URL_MODULES', self.url_to_modules)
+                self.config.set('STRATOSPHERE_WINDOW', 'UPDATE_URL_MODELS', self.url_to_models)
+                self.config.set('STRATOSPHERE_WINDOW', '; LENGTH OF STATE FOR EACH TUPLE.')
+                self.config.set('STRATOSPHERE_WINDOW', 'LENGTH_OF_STATE', self.length_of_state)
+                self.config.set('STRATOSPHERE_WINDOW', '; TIME IN SECONDS FOR ANALYZE TUPLES')
+                self.config.set('STRATOSPHERE_WINDOW', 'TIME_WINDOWS_LENGTH', self.time_windows_length)
+
                 # SECOND SECTION
-                config.add_section('STRATOSPHERE_WATCHER')
-                config.set('STRATOSPHERE_WATCHER', 'RUN_ON_START_UP', self.run_on_start)
+                self.config.add_section('STRATOSPHERE_WATCHER')
+                self.config.set('STRATOSPHERE_WATCHER', 'RUN_ON_START_UP', self.run_on_start)
                 # THIRD SECTION
-                config.add_section('STRATOSPHERE_OUTPUT')
-                config.set('STRATOSPHERE_OUTPUT', 'VERBOSE_OPTION', self.verbose_option)
+                self.config.add_section('STRATOSPHERE_OUTPUT')
+                self.config.set('STRATOSPHERE_OUTPUT', '; IT IS NUMBER FROM 0 TO 3. 0=NOTHING PRINTED TO CONSOL, 3=EVERYTHING IS PRINTED TO CONSOL.')
+                self.config.set('STRATOSPHERE_OUTPUT', 'VERBOSE_OPTION', self.verbose_option)
 
                 with open(file_name, 'wb') as configfile:
-                    config.write(configfile)
+                    self.config.write(configfile)
 
         # Set new date for uodate (default is today + 3 days)
         def set_config(self):
@@ -80,11 +89,10 @@ class StratosphereConfig:
             next_update = datetime.date.today() + datetime.timedelta(self.days_update_again)
 
             file_name = 'configfile.cfg'
-            config = ConfigParser.RawConfigParser()
-            config.read(file_name)
-            config.set('STRATOSPHERE_WINDOW', 'DATE_OF_NEXT_UPDATE', next_update)
+            self.config.read(file_name)
+            self.config.set('STRATOSPHERE_WINDOW', 'DATE_OF_NEXT_UPDATE', next_update)
             with open(file_name, 'wb') as configfile:
-                config.write(configfile)
+                self.config.write(configfile)
 
 
 

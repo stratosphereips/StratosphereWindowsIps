afdde88e00b9603d52a58d61ff6b472d7f7c46b7
Frenky Střasák <Frenky Stask>
2016-02-25 02:22:12 +0100

    Add logging.

--- StratosphereWindowsIPS/StratosphereOutput.py
+++ StratosphereWindowsIPS/StratosphereOutput.py
@@ -1,13 +1,29 @@
 __author__ = 'Frenky'
 
+import logging
+import datetime
+
 config_instance = None
 
 
 def import_instance():
+    # import 'config_instance', because we need to know verbose option from config file.
     global config_instance
     config_instance = __import__('StratosphereConfig').StratosphereConfig.config_instance
+    # Logging
+    logging.basicConfig(filename='LogFile.log', level=logging.DEBUG)
 
 
 def show(text1, option):
     if option <= config_instance.verbose_option:
-        print text1
+        if option == 1:
+            print text1
+        elif option == 2:
+            print '      ', text1
+        elif option == 3:
+            print '             ', text1
+
+
+# Wrtite to log file next message.
+def log(message):
+    logging.debug(str(datetime.datetime.now()) + ' -- ' + message)

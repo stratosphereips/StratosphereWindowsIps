2adc2257e105ca853276ce962abf98afacfcd705
frenkypicasso <frenky.picasso@seznam.cz>
2016-03-07 13:56:49 +0100

    Delete wrong singleton.

--- StratosphereWindowsIPS/StratosphereOutput.py
+++ StratosphereWindowsIPS/StratosphereOutput.py
@@ -2,20 +2,13 @@ __author__ = 'Frenky'
 
 import logging
 import datetime
+from StratosphereConfig import __StratosphereConfig__
 
-config_instance = None
-
-
-def import_instance():
-    # import 'config_instance', because we need to know verbose option from config file.
-    global config_instance
-    config_instance = __import__('StratosphereConfig').StratosphereConfig.config_instance
-    # Logging
-    logging.basicConfig(filename='LogFile.log', level=logging.DEBUG)
+logging.basicConfig(filename='LogFile.log', level=logging.DEBUG)
 
 
 def show(text1, option):
-    if option <= config_instance.verbose_option:
+    if option <= __StratosphereConfig__.get_int_verbose_option():
         if option == 1:
             print text1
         elif option == 2:

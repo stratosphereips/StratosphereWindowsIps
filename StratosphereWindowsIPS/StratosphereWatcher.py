09fc8c4e5b49b773056e68fca92abcfd6154a7b2
frenkypicasso <frenky.picasso@seznam.cz>
2016-03-18 03:58:28 +0100

    Exception for no internet acces, path to files is in program arguments.

--- StratosphereWindowsIPS/StratosphereWatcher.py
+++ StratosphereWindowsIPS/StratosphereWatcher.py
@@ -3,7 +3,9 @@ import subprocess
 import time
 import datetime
 import urllib2
+import urllib
 import zipfile
+import sys
 from StratosphereConfig import __StratosphereConfig__
 import StratosphereOutput
 
@@ -40,17 +42,46 @@ def unzips(name):
         z.extract(i, "./" + name)
     fh.close()
 
+# Function try to connect to internet.
+def is_connected(host):
+    try:
+        urllib.urlopen(host)
+        return True
+    except:
+        return False
+
+# Can we download everything what we need?
+def can_we_download():
+    # Try by google if user is conected to the internet.
+    if is_connected('http://google.com') is False:
+        StratosphereOutput.show('No internet acces!!!', 1)
+        return False
+    # Try to connect to github for the files.
+    if is_connected(__StratosphereConfig__.get_string_url_to_classes()) is False:
+        StratosphereOutput.show('We are not able to conect to ' + __StratosphereConfig__.get_string_url_to_classes() + '!!!', 1)
+        return False
+    # Try to connect to fel.cvut for files.
+    if is_connected(__StratosphereConfig__.get_string_url_to_models()) is False:
+        StratosphereOutput.show('We are not able to conect to ' + __StratosphereConfig__.get_string_url_to_models() + '!!!', 1)
+        return False
+    if is_connected(__StratosphereConfig__.get_string_url_to_modules()) is False:
+        StratosphereOutput.show('We are not able to conect to ' + __StratosphereConfig__.get_string_url_to_modules() + '!!!', 1)
+
+    # If everything is correct, return True.
+    return True
+
 
 def download_manager():
-    StratosphereOutput.show("Downloading new update.", 1)
-    # Download classes StratosphereFlow, StratosphereWindow..
-    download_file(__StratosphereConfig__.get_string_url_to_classes())
-    # Download Models
-    download_file(__StratosphereConfig__.get_string_url_to_models())
-    unzips('models')
-    # Download Modules
-    download_file(__StratosphereConfig__.get_string_url_to_modules())
-    unzips('modules')
+    if can_we_download():
+        StratosphereOutput.show("Downloading new update.", 1)
+        # Download classes StratosphereFlow, StratosphereWindow..
+        download_file(__StratosphereConfig__.get_string_url_to_classes())
+        # Download Models
+        download_file(__StratosphereConfig__.get_string_url_to_models())
+        unzips('models')
+        # Download Modules
+        download_file(__StratosphereConfig__.get_string_url_to_modules())
+        unzips('modules')
 
 
 def check_if_update():
@@ -71,11 +102,13 @@ if __name__ == "__main__":
     # checking if
     check_if_update()
 
-    # Path to Stratospehere Window
-    path = 'C:\\Users\\frenk\\Documents\\Skola\\Stratosphere\\StratosphereWindowsIPS'
+    # Path to StratospehereFlow
+    path_to_StrapFlow = sys.argv[1]
+    # Path to binet flows
+    path_to_binetflow = sys.argv[2]
 
     # Create process.
-    command = ('cat ' + path + '\\test8.binetflow | python ' + path + '\\StratosphereFlow.py')
+    command = ('cat ' + path_to_binetflow + ' | python ' + path_to_StrapFlow)
     p = subprocess.Popen(command, shell=True)
 
     # Wait until process terminates.
@@ -84,7 +117,7 @@ if __name__ == "__main__":
             StratosphereOutput.show("Process is still runnning...", 3)
             time.sleep(__StratosphereConfig__.get_int_check_if_process_work())
         else:
-            # This is coment for testig mode.
+            # This is coment for testing mode.
             # p = subprocess.Popen(command, shell=True)
             pass
     StratosphereOutput.show(("Process ended, ret code:", p.returncode), 2)

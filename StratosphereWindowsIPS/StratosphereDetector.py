1225732a3f261eb2ee5b4b96b31ca26a80f8b0a8
frenkypicasso <frenky.picasso@seznam.cz>
2016-04-12 00:53:43 +0200

    Calling markov_models.py and setting markov_model.py.

--- StratosphereWindowsIPS/StratosphereDetector.py
+++ StratosphereWindowsIPS/StratosphereDetector.py
@@ -2,18 +2,22 @@ __author__ = 'Frenky'
 import random
 from modules.markov_models_1 import __markov_models__
 
+# set path into the models
+__markov_models__.set_models_folder('./models')
+
 
 def detect(tuple):
-    __markov_models__.set_models_folder('C:\\Users\\frenk\\Documents\\Skola\\Stratosphere\\StratosphereWindowsIPS\\models')
-    #----------------------------------------
-    #----------- Real option -------------------
+
+
+    # #----------------------------------------
+    # #----------- Real option -------------------
     # Analyze state a return name of the result.
     (detected, label, matching_len) = __markov_models__.detect(tuple, 50)
     return (detected, label, matching_len)
 
-    # # ----------------------------------------
-    # # ------------ Test option ---------------
-    # # Random label
+    # ----------------------------------------
+    # ------------ Test option ---------------
+    # Random label
     # label_number = random.randint(1, 16)
     # if label_number is 1:
     #     return ('', 'Botnet', '')
